0ccb87275d9722df02d1d81ea7c140b291c2424c
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2022-12-08 19:53:51 +0530

    #include<stdio.h> int main() {     int p,q,r,s;     scanf("%d%d%d%d",&p,&q,&r,&s);     if(p>=10&&q>=10&&r>=10&&s>=10)     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Target.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int p,q,r,s;
+    scanf("%d%d%d%d",&p,&q,&r,&s);
+    if(p>=10&&q>=10&&r>=10&&s>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
