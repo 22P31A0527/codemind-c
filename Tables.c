3ba38a3b24aa98162dc41ae010f5411f34be2aaa
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2022-12-11 09:40:48 +0530

    #include<stdio.h> int main() {     int n,m,i,s;     scanf("%d%d",&n,&m);     for(i=1;i<=m;i++)     {         if(i%2!=0)         {             printf("%d x %d = %d ",n,i,n*i);         }     }
    }

---
+++ Tables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,s;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<=m;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d x %d = %d
+",n,i,n*i);
+        }
+    }
+    
+    
+}
