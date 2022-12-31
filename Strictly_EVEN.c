517f6a91e7c4b7c442d941ead11c2d3a446bb2d0
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2022-12-31 05:57:58 +0530

    #include<stdio.h> int main() {     int n,a[n],i,c;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             if(i%2==0)             {                 c=0;             }             else             {                 c=1;                 break;             }         }     }     if(c==0)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,c=0;
+    int n,a[n],i,c;
     scanf("%d",&n);
-    int a[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
@@ -23,7 +22,7 @@ int main()
             }
         }
     }
-    if(c<1)
+    if(c==0)
     {
         printf("True");
     }
