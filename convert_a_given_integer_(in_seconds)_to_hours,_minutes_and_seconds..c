de9c4f2e81a1fc470d9dfd375ddae7f688c94d2f
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-01-04 18:53:38 +0530

    #include<stdio.h> int main() {     int n,h,m,s;     scanf("%d",&n);     h=n/3600;     m=(n%3600)/60;     s=(n%3600)%60;     printf("H:M:S-%d:%d:%d",h,m,s); }

--- convert_a_given_integer_(in_seconds)_to_hours,_minutes_and_seconds..c
+++ convert_a_given_integer_(in_seconds)_to_hours,_minutes_and_seconds..c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    scanf("%d",&a);
-    int x,y,z,m;
-    x=a/3600;
-    y=a%3600;
-    z=y/60;
-    m=y%60;
-    printf("H:M:S-%d:%d:%d",x,z,m);
+    int n,h,m,s;
+    scanf("%d",&n);
+    h=n/3600;
+    m=(n%3600)/60;
+    s=(n%3600)%60;
+    printf("H:M:S-%d:%d:%d",h,m,s);
 }
