2cb67229e23188196998a6aea6fc9125c3bc638a
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-01-07 09:03:15 +0530

    #include<stdio.h> int main() {     int n,s;     scanf("%d",&n);     s=n%9;     printf("%d",s); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,18 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,s=0;
+    int n,s;
     scanf("%d",&n);
-    while(n>=9)
-    {
-        s=0;
-        while(n>0)
-        {
-            r=n%10;
-            s=s+r;
-            n=n/10;
-        }
-        n=s;
-    }
-    printf("%d",n);
+    s=n%9;
+    printf("%d",s);
 }
