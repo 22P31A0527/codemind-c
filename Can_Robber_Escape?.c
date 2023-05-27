ed3a97ef48bf52bc89aefc4f12a0ef1a26c16648
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-05-27 15:20:31 +0530

    #include<stdio.h> int main() {     int n,i,a[n],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             c++;         }     }     if(c<=2) printf("YES");     else printf("NO"); }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[n],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            c++;
+        }
+    }
+    if(c<=2) printf("YES");
+    else printf("NO");
+}
