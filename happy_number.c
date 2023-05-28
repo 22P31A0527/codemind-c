3eb5c311a92c74e5e00d1a59fe0c828fec003a36
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-05-28 09:58:34 +0530

    #include<stdio.h> int fun(int n) {     int r,s=0;     while(n!=0)     {         r=n%10;         s=s+r*r;         n=n/10;     }     if(s<=9) return s;     fun(s); } int main() {     int n,m;     scanf("%d",&n);     m=fun(n);     if(m==1 || m==7)printf("True");     else printf("False"); }

---
+++ happy_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fun(int n)
+{
+    int r,s=0;
+    while(n!=0)
+    {
+        r=n%10;
+        s=s+r*r;
+        n=n/10;
+    }
+    if(s<=9) return s;
+    fun(s);
+}
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    m=fun(n);
+    if(m==1 || m==7)printf("True");
+    else printf("False");
+}
