c750f52295c676156521c0680c96de3bb272dce7
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-06 18:11:28 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int j,c;     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[j]==a[i])             {                 c++;             }         }         if(c>1)         {             printf("%d",a[i]);             break;         }     } }

---
+++ Find_the_Duplicate_Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int j,c;
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[j]==a[i])
+            {
+                c++;
+            }
+        }
+        if(c>1)
+        {
+            printf("%d",a[i]);
+            break;
+        }
+    }
+}
