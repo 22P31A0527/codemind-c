afae2506990ea4f9cb96608a653d6b75c3c4667d
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-06 10:45:08 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n],b[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int m=n/2,j=0;     for(i=n-1;i>=m;i--)     {         b[j]=a[i];         j++;     }     for(i=0;i<m;i++)     {         b[j]=a[i];         j++;     }     for(i=0;i<n;i++)     {         printf("%d ",b[i]);     } }

--- Half_List.c
+++ Half_List.c
@@ -3,18 +3,24 @@ int main()
 {
     int n,i;
     scanf("%d",&n);
-    int a[n];
+    int a[n],b[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    int m=n/2;
+    int m=n/2,j=0;
     for(i=n-1;i>=m;i--)
     {
-        printf("%d ",a[i]);
+        b[j]=a[i];
+        j++;
     }
     for(i=0;i<m;i++)
     {
-        printf("%d ",a[i]);
+        b[j]=a[i];
+        j++;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",b[i]);
     }
 }
