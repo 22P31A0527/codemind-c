56ff6988790f35f7e484796ffed50a71ac079163
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-12 12:33:26 +0530

    #include<stdio.h> int rec(int n) {     int r,s=0;     while(n!=0)     {         r=n%10;         s=s+r*r;         n=n/10;     }     if(s<=9) return s;     rec(s); } int main() {     int n;     scanf("%d",&n);     int x=rec(n);     if(x==1 ||  x==7) printf("True");     else printf("False"); }

--- happy_number.c
+++ happy_number.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int fun(int n)
+int rec(int n)
 {
-    int i,r,s=0;
+    int r,s=0;
     while(n!=0)
     {
         r=n%10;
@@ -9,13 +9,13 @@ int fun(int n)
         n=n/10;
     }
     if(s<=9) return s;
-    fun(s);
+    rec(s);
 }
 int main()
 {
-    int n,m;
+    int n;
     scanf("%d",&n);
-    m=fun(n);
-    if(m==1 || m==7) printf("True");
+    int x=rec(n);
+    if(x==1 ||  x==7) printf("True");
     else printf("False");
 }
