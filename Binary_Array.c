bdce9a6b6541146be3f12825158f0a55cf02d398
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-12 12:14:52 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int s=1;     for(i=0;i<n;i++)     {         if(a[i]!=0 && a[i]!=1)         {             s=0;             break;         }     }     if(s==0) printf("False");     else printf("True"); }

--- Binary_Array.c
+++ Binary_Array.c
@@ -1,24 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,a[n],s=0;
+    int n,i;
     scanf("%d",&n);
+    int a[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
+    int s=1;
     for(i=0;i<n;i++)
     {
-        if(a[i]==0 || a[i]==1)
-        {
-            s=1;
-        }
-        else
+        if(a[i]!=0 && a[i]!=1)
         {
             s=0;
             break;
         }
     }
-    if(s==1) printf("True");
-    else printf("False");
+    if(s==0) printf("False");
+    else printf("True");
 }
