8a7686503fb196e1dd085360676312ac58ac4010
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-15 19:14:21 +0530

    #include<stdio.h> int main() {     int n,m,i,j;     scanf("%d%d",&n,&m);     int a[n][m];     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }     int s;     for(i=0;i<n;i++)     {         s=0;         for(j=0;j<m;j++)         {             s=s+a[i][j];         }         printf("%d ",s);     } }

--- Row_wise_sum_of_a_matrix.c
+++ Row_wise_sum_of_a_matrix.c
@@ -11,14 +11,14 @@ int main()
             scanf("%d",&a[i][j]);
         }
     }
-    int sum;
+    int s;
     for(i=0;i<n;i++)
     {
-        sum=0;
+        s=0;
         for(j=0;j<m;j++)
         {
-            sum=sum+a[i][j];
+            s=s+a[i][j];
         }
-        printf("%d ",sum);
+        printf("%d ",s);
     }
 }
