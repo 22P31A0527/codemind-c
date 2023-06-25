c80c7adbb7499d656b1c34dca061abf0d5bdcacc
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-25 12:35:13 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n],b[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         scanf("%d",&b[i]);     }     int c[n];     for(i=0;i<n;i++)     {         c[i]=a[i]+b[i];         printf("%d ",c[i]);     } }

---
+++ Monk_and_Welcome_Problem.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    int c[n];
+    for(i=0;i<n;i++)
+    {
+        c[i]=a[i]+b[i];
+        printf("%d ",c[i]);
+    }
+}
