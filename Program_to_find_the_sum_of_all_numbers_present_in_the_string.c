2de8e47faad5461c52ab18641156bcd62b82a80b
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-07-08 20:26:47 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[100];     scanf("%s",&s);     int i,x,y,sum=0;     for(i=0;i<strlen(s);i++)     {         if(s[i]=='1'||s[i]=='2'||s[i]=='3'||         s[i]=='4'||s[i]=='5'||s[i]=='6'||         s[i]=='7'||s[i]=='8'||s[i]=='9')         {             x=s[i];             y=x-48;             sum=sum+y;         }     }     printf("%d",sum); }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100];
+    scanf("%s",&s);
+    int i,x,y,sum=0;
+    for(i=0;i<strlen(s);i++)
+    {
+        if(s[i]=='1'||s[i]=='2'||s[i]=='3'||
+        s[i]=='4'||s[i]=='5'||s[i]=='6'||
+        s[i]=='7'||s[i]=='8'||s[i]=='9')
+        {
+            x=s[i];
+            y=x-48;
+            sum=sum+y;
+        }
+    }
+    printf("%d",sum);
+}
